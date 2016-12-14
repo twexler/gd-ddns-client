9ba335e5c2e0010274c22d15ad5295388f143007
Ted Wexler <ted@stuckinacan.com>
2016-12-14 11:14:56 -0800

    Add zap, use it

--- cmd/gd-ddns-client.go
+++ cmd/gd-ddns-client.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"flag"
-	"log"
 
-	"github.com/twexler/gd-ddns-client"
+	"github.com/uber-go/zap"
+
+	client "github.com/twexler/gd-ddns-client"
 )
 
 var (
 	configFile string
+	logger     zap.Logger
 )
 
 func init() {
 	flag.StringVar(&configFile, "configFile", "/etc/gd-ddns-client.yaml", "spacify the config file to be used")
+	logger = zap.New(zap.NewTextEncoder())
 }
 
 func main() {
-	conf, err := gdDDNSClient.NewConfigFromFile(configFile)
+	conf, err := client.NewConfigFromFile(configFile)
 	if err != nil {
-		log.Fatalf("Unable to read config file %s: %s", configFile, err.Error())
+		logger.Fatal("Unable to read config file", zap.String("configFile", configFile), zap.Error(err))
 	}
+
 }
